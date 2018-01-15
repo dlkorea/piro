8a2ce34634092f56869311128a4f91f711d9cd53
Kaske MacBook <bright14u@gmail.com>
2018-01-15 16:36:19 +0900

    template extends and blocks

--- piro_blog/piro_blog/settings.py
+++ piro_blog/piro_blog/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'piro_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'piro_blog', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
